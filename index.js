1d61336c3093ad1b58b8f01d4d34dcce6783bd38
SkynI25 <rlxo1651@daum.net>
2020-04-07 23:43:41 +0900

    add QNA features

--- index.js
+++ index.js
@@ -186,7 +186,7 @@
 
     function addStores(data) {
         let storeDatas = sortingDataByType(JSON.parse(data));
-        let pharmacyCount = document.querySelectorAll('.content .pharmacy-total h2')[0].textContent + '총 ' + storeDatas.count + '개 지점';
+        let pharmacyCount = document.querySelectorAll('.content .pharmacy-total h2')[0].textContent + ' 총 ' + storeDatas.count + '개 지점';
         pharmacyCount += `\n(현재 : ${isAvailableStock(storeDatas.stores)}개 지점 보유중)`
         document.querySelectorAll('.content .pharmacy-total h2')[0].textContent = pharmacyCount;
 
@@ -231,7 +231,152 @@
         console.warn(`ERROR(${err.code}): ${err.message}`);
     }
 
+    function qnaClick() {
+        const aList = document.querySelectorAll('.faq-list li a');
+        for(let a of aList) {
+            a.addEventListener('click', evt => {
+                evt.preventDefault();
+                evt.stopPropagation();
+                let setAriaExpanded = a.children[1].children[0].getAttribute('aria-expanded') === "false" ? "true" : "false";
+                a.children[1].children[0].setAttribute('aria-expanded', setAriaExpanded);
+                let isDisplay = a.parentElement.children[1].style.display;
+                a.parentElement.children[1].style.display = isDisplay === 'block' ? 'none' : 'block';
+            });
+        }
+    }
+
+    function menuClick() {
+        document.querySelectorAll('ul > li > .FAQ')[0].addEventListener('click', evt => {
+            evt.preventDefault();
+            document.querySelectorAll('.content')[0].innerHTML = `
+            <div class="faq-title">
+            <h3>FAQ</h3>
+        </div>
+        <ul class="faq-list">
+            <li>
+                <a href="#" role="button">
+                    <div class="q-content">
+                        <span class="q-text">Q.</span>
+                        <span>위치 및 재고 업데이트 버튼을 눌러도 목록이 안 떠요</span>
+                    </div>
+                    <div class="arrow-sign">
+                        <img class="q-sign" src="resource/qsign.png" aria-expanded="false">
+                    </div>
+                </a>
+                <div class="a-content">
+                    <div class="a-text">
+                        <div class="a-mobile">
+                            모바일)
+                        </div>
+                        <p>1단계 : 휴대폰의 위치를 킨다.</p>
+                        <p>2단계 : 사용하는 브라우저의 캐시를 지운다.</p>
+                        <p>3단계 : 휴대폰 설정 > 애플리케이션 > Chrome > 앱 권한으로 들어가서 위치 권한을 허용해준다. (이미 허용되어 있는 경우 이 단계는 무시)</p>
+                        <p>4단계 : 사이트에 재접속해서 '위치 및 재고 업데이트' 버튼을 누르면 '기기의 위치에 액세스하려고 합니다' 라는 팝업이 뜬다.</p>
+                        <p>'허용' 버튼을 클릭해준다.</p>
+    
+                        <div class="another-solution">(위 방법으로도 안되는 경우)</div>
+                        <p>Galaxy Q6 기종의 경우 위 방법으로 해결이 안되는 것으로 확인되었습니다.<br>
+                        이 경우 play 스토어에서 새로운 모바일 브라우저(예: firefox)를 설치하셔서 사이트에 접속하시길 권장드립니다.</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#" role="button">
+                    <div class="q-content">
+                        <span class="q-text">Q.</span>
+                        <span>판매처는 약국만 있나요?</span>
+                    </div>
+                    <div class="arrow-sign">
+                        <img class="q-sign" src="resource/qsign.png" aria-expanded="false">
+                    </div>
+                </a>
+                <div class="a-content">
+                    <div class="a-text">
+                        <p>현재 정부에서 제공하는 데이터로는 약국, 우체국, 하나로 마트 총 3개의 판매처에서 마스크를 판매하는 것으로 확인되었습니다.</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#" role="button">
+                    <div class="q-content">
+                        <span class="q-text">Q.</span>
+                        <span>현재 위치로만 검색할 수 밖에 없나요?</span>
+                    </div>
+                    <div class="arrow-sign">
+                        <img class="q-sign" src="resource/qsign.png" aria-expanded="false">
+                    </div>
+                </a>
+                <div class="a-content">
+                    <div class="a-text">
+                        <p>주소를 입력할 시 해당 주소에 위치한 약국 데이터를 보여주는 작업을 현재 진행 중이며, 4월 중순에 서비스 될 예정입니다.</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <a href="#" role="button">
+                    <div class="q-content">
+                        <span class="q-text">Q.</span>
+                        <span>위치 정보를 제공하면 어떻게 되나요?</span>
+                    </div>
+                    <div class="arrow-sign">
+                        <img class="q-sign" src="resource/qsign.png" aria-expanded="false">
+                    </div>
+                </a>
+                <div class="a-content">
+                    <div class="a-text">
+                        <p>해당 위치 정보를 통해 근처에 있는 판매처 목록을 보여줍니다. 위치 정보는 따로 저장되거나 절대 악용되지 않습니다.</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+        <div class="service-provider">
+            <h3>원하시는 정보를 찾지 못하셨나요?</h3>
+            <p>아래 E-mail로 연락을 주시면 친절히 안내해드리겠습니다.</p>
+            <p><a href="mailto:rlxo1651@naver.com">E-mail : rlxo1651@naver.com</a></p>
+        </div>
+            `;
+            qnaClick();
+        }, false);
+    
+        document.querySelectorAll('ul > li > .store')[0].addEventListener('click', evt => {
+            evt.preventDefault();
+            document.querySelectorAll('.content')[0].innerHTML = `
+            <div class="pharmacy-total">
+                <h2 class="pharmacy-count">판매처 : </h2>
+                <div class="pharmacy-data">
+                    <div class="select-distance">
+                        <select id="distance-rad">
+                            <option value="500">반경 500m</option>
+                            <option value="1000">반경 1000m</option>
+                            <option value="1500">반경 1500m</option>
+                            <option value="2000">반경 2000m</option>
+                            <option value="3000">반경 3000m</option>
+                            <option value="4000">반경 4000m</option>
+                            <option value="5000">반경 5000m</option>
+                        </select>
+                    </div>
+                    <div class="sort-type">
+                        <input type="radio" id="distance" name="sort_type" value="distance" checked="checked">
+                        <label for="distance">거리순</label>
+                        <input type="radio" id="stock" name="sort_type" value="stock">
+                        <label for="stock">재고순</label>
+                    </div>
+                    <button id="update">위치 및 재고<br>업데이트</button>
+                </div>
+            </div>
+            <ul class="pharmacy-list">
+                <p>현재 주변의 마스크 판매처를 찾으시려면 위에서 거리를 선택 후 업데이트 버튼을 클릭해주세요.<br>
+            메신저로 접근하셨다면 우측 상단의 설정을 누른 뒤 <span class="pointout">다른 브라우저로 열기</span>를 클릭해주세요.</p>
+            </ul>
+            `;
+            document.querySelectorAll('#update')[0].addEventListener('click', evt => {
+                navigator.geolocation.getCurrentPosition(success, error, options);
+            }, false);
+        }, false);
+    };
+
     document.querySelectorAll('#update')[0].addEventListener('click', evt => {
         navigator.geolocation.getCurrentPosition(success, error, options);
     }, false);
+    menuClick();
 })();
